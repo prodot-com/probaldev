9cb6e3e8b68335514f601dd4cce9f1a3bc5c3c46
Probal Ghosh <probalghoshofficial@gmail.com>
2025-12-03 01:28:50 +0530

    Slight change

--- src/App/Page.tsx
+++ src/App/Page.tsx
@@ -139,9 +139,9 @@ const Home = () => {
           </div>
 
           <div className=" mt-7 pt-5 pl-4 md:px-9 border-t-1 dark:border-neutral-700 flex flex-col gap-3 w-full max-w-5xl min-h-[170px]">
-            <p className="pl-2 flex items-center gap-1 hanken-grotesk-font">
+            <p className="pl-2 flex items-center gap-1">
               <Copyright className="w-5 sm:w-5"/> 
-              <span className="instrument-serif-bold text-[19px] sm:text-[19px]">2025 Probal Ghosh. All rights reserved.</span>
+              <span className="instrument-serif text-[19px] sm:text-[19px]">2025 Designed & Developed by <span className="text-[21px] instrument-serif-bold">Probal</span></span>
             </p>
           </div>
 
