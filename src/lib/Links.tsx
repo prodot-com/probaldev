6508678c0b0115c427f8797f44a98a029cf7d350
Probal Ghosh <probalghoshofficial@gmail.com>
2025-12-29 23:04:15 +0530

    fix: Update ResumeLink to use the correct sharing URL

--- src/lib/Links.tsx
+++ src/lib/Links.tsx
@@ -1,4 +1,4 @@
 export const XLink =  "https://x.com/"
 export const LinkedInLink =  "https://www.linkedin.com/in/ghoshprobal/"
 export const GithUbLink =  "https://github.com/prodot-com"
-export const ResumeLink =  "https://drive.google.com/file/d/1Y4Ej810_PEJ0NJe9srJIlldKgi0_J6G1/view?usp=drive_link"
+export const ResumeLink =  "https://drive.google.com/file/d/1Y4Ej810_PEJ0NJe9srJIlldKgi0_J6G1/view?usp=sharing"
